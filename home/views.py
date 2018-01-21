9dce7d37482dfae545c66a015b730e29e3559bea
Alexandre Erich Sébastien Georges <erichaitian97@gmail.com>
2018-01-21 09:02:18 -0500

    more setup and homepage

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+index_template = 'home/index.html'
+
+
+def index(request):
+    title = 'Byenvini sou Zafè Sante'
+    message = 'Se mèt ko ki veye ko.'
+    context = {'title': title, 'message': message}
+    return render(request, index_template, context)
