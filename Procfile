333927dee67bd49e4ce93738432954efbee27f4b
Alexandre Erich Sébastien Georges <erichaitian97@gmail.com>
2018-01-21 08:30:39 -0500

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn zafesante.wsgi --log-file -
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alexan~1\docume~1\github\zafesante\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
