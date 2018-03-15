03ee5de143e1b801b2030b3c79024380e2a7a1a5
Alexandre Erich Sébastien Georges <erichaitian97@gmail.com>
2018-03-15 13:47:38 -0400

    wrappingup#2

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 index_template = 'home/index.html'
+register_template = 'registration/register.html'
 
 
 def index(request):
@@ -10,3 +13,18 @@ def index(request):
     message = 'Se mèt ko ki veye ko.'
     context = {'title': title, 'message': message}
     return render(request, index_template, context)
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('questions')
+    else:
+        form = UserCreationForm()
+    return render(request, register_template, {'form': form})
