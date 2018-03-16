1e864da4bba243cee775671ec2954d39bb12c5bc
Alexandre Erich Sébastien Georges <erichaitian97@gmail.com>
2018-03-16 10:30:22 -0400

    bug#6

--- zafesante/settings.py
+++ zafesante/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'home',
     'media',
     'questions',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -141,7 +142,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-AWS_LOCATION = ''
+AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN_STATIC, AWS_LOCATION)
 
