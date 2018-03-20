a094258473ea69d82ecc8a5aaaf684417251adbf
Alexandre Erich Sébastien Georges <erichaitian97@gmail.com>
2018-03-19 21:50:05 -0400

    share articles

--- zafesante/settings.py
+++ zafesante/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*', 'zafe-sante.herokuapp.com', 'www.zafesante.com']
 
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'home',
     'media',
     'questions',
+    #3rd party
     'storages',
+    'django_social_share',
 ]
 
 MIDDLEWARE = [
