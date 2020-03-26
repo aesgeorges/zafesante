c888ffed86bac49462c06b6597421fbbfca05269
Alexandre Erich Sébastien Georges <alexandre.georges@stonybrook.edu>
2020-03-26 13:13:42 -0400

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
-    path('signup/', views.signup, name='signup'),
+    #path('login/', auth_views.login, name='login'),
+    #path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+    #path('signup/', views.signup, name='signup'),
 ]
